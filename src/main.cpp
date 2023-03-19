a9d0f9eac24efa21814358283e9d45bca960fd2e
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-03-18 23:33:01 -0700

    Open a window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,41 @@
-#include <iostream>
-#include <SDL2/SDL.h> 
+#include <SDL2/SDL.h>
+#include <stdio.h>
 
-int main(){
+#define SCREEN_WIDTH 1280 
+#define SCREEN_HEIGHT 720
 
-  if(SDL_Init(SDL_INIT_EVERYTHING) > 0){
-    std::cout << "Shit failed" << std::endl; 
-  }
+int main(int argc, char** argv){
+    if(SDL_Init(SDL_INIT_VIDEO) < 0){
+        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
 
-  std::string hello = "Hello world"; 
-  std::cout << hello << std::endl; 
-  SDL_Quit(); 
-  return 0; 
+    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+    if(!window){
+        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if(!renderer){
+        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+        return 1;
+    }
+
+    bool running = true;
+    while(running){
+        SDL_Event event;
+        while(SDL_PollEvent(&event)){
+          if (event.type == SDL_QUIT){
+            running = false; 
+          }
+        }
+
+        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
+        SDL_RenderClear(renderer);
+
+        SDL_RenderPresent(renderer);
+    }
+
+    return 0;
 }
