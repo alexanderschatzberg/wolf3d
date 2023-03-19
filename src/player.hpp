cb0fc55e0a05856dfa0526b316ca135d822ee297
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-03-19 15:39:23 -0700

    Add a player height

--- src/player.hpp
+++ src/player.hpp
@@ -12,6 +12,10 @@ struct Keys {
   bool w, a, s, d;
 };
 
+struct Coord {
+  float x, y;
+};
+
 class Player {
 public:
   SDL_Color color;
@@ -25,7 +29,7 @@ public:
   std::vector<Ray> rays;
   float fov;
 
-  Player(SDL_Renderer* renderer, World* world, float x, float y, int w, int h, SDL_Color color, int numRays, float fov) {
+  Player(SDL_Renderer* renderer, World* world, float x, float y, int w, int h, SDL_Color color, int numRays, float fov, int height) {
     this->renderer = renderer;
     this->world = world;
     this->color = color;
@@ -39,28 +43,41 @@ public:
     }
   }
 
-  void updateRayAngles(float fov) {
+  void castRays(float fov) {
     float offset = (this->fov) / 2; float startingPoint = this->theta - offset; float endingPoint = this->theta + offset;
     float range = endingPoint - startingPoint; float step = range / this->rays.size();
     for (float i = startingPoint; i < endingPoint; i += step) {
       float x2 = cos(i) * 20;
       float y2 = sin(i) * 20;
       SDL_RenderDrawLine(renderer, (int)x + width / 2, (int)y + height / 2, (int)x + width / 2 + x2 * 32, (int)y + height / 2 + y2 * 32);
+
     }
   }
 
-  void castRays(int numRays) {
+  Coord getPositionInMap(float x, float y) {
+    Coord position;
+    position.x = (x / 64);
+    position.y = (y / 64);
+    return position;
+  }
+
+  void checkHorizontalCast(float theta, float x, float y) {
+    if (theta > PI) {
+
+    }
+  }
+
+  void checkVerticalCast(float theta, float x, float y) {
+    //TODO
 
   }
+
 
   void draw() {
     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
     SDL_Rect rect = { (int)x, (int)y, width, height };
     SDL_RenderFillRect(renderer, &rect);
-    //Draw a line to show the direction, numbers are arbitrary
-    SDL_RenderDrawLine(renderer, (int)x + width / 2, (int)y + height / 2, (int)x + width / 2 + dx * 13, (int)y + height / 2 + dy * 13);
-    updateRayAngles(this->fov);
-    std::cout << this->theta << std::endl;
+    castRays(this->fov);
   }
 
   void move(Uint32 deltaTime) {
