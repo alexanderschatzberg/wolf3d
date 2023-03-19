414d8b4653e25d368f83c4432678f935de103ec6
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-03-19 03:15:00 -0700

    2d player movement

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,76 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+#include "player.hpp"
+
 #define SCREEN_WIDTH 1280 
 #define SCREEN_HEIGHT 720
 
-int main(int argc, char** argv){
-    if(SDL_Init(SDL_INIT_VIDEO) < 0){
-        printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
+int main(int argc, char** argv) {
+  //Initialize SDL
+  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+    printf("Error: SDL failed to initialize\nSDL Error: '%s'\n", SDL_GetError());
+    return 1;
+  }
+
+  //Create window
+  SDL_Window* window = SDL_CreateWindow("WOLf 3D", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
+  if (!window) {
+    printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
+    return 1;
+  }
+  //Create renderer to draw on
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+  if (!renderer) {
+    printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
+    return 1;
+  }
+
+  //Create player
+  float x = (float)SCREEN_WIDTH / 2, y = (float)SCREEN_HEIGHT / 2;
+  int width = 15, height = 15;
+  Player player = Player(renderer, (int)x, (int)y, width, height, { 255, 0, 0, 255 });
+
+  //Time variables for delta time
+  Uint32 lastTime = 0, currentTime = 0;
+
+  bool running = true;
+  //Main loop
+  while (running) {
+    //Calculate delta time
+    currentTime = SDL_GetTicks64();
+    float deltaTime = (currentTime - lastTime);
+    lastTime = currentTime;
+
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_QUIT) {
+        std::cout << "Quit" << std::endl;
+        running = false;
+      }
+      //if the keyboard is being pressed, update the player's key state 
+      if (event.type == SDL_KEYDOWN) {
+        player.updateKeys(event.key.keysym.sym, true);
+      }
+      if (event.type == SDL_KEYUP) {
+        player.updateKeys(event.key.keysym.sym, false);
+      }
     }
 
-    SDL_Window *window = SDL_CreateWindow("SLD test", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
-    if(!window){
-        printf("Error: Failed to open window\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    SDL_SetRenderDrawColor(renderer, 160, 160, 160, 255); //Paint background grey
+    SDL_RenderClear(renderer); //Clear Canvas
 
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if(!renderer){
-        printf("Error: Failed to create renderer\nSDL Error: '%s'\n", SDL_GetError());
-        return 1;
-    }
+    //Move the player according to the keyboard input
+    player.move(deltaTime);
 
-    bool running = true;
-    while(running){
-        SDL_Event event;
-        while(SDL_PollEvent(&event)){
-          if (event.type == SDL_QUIT){
-            running = false; 
-          }
-        }
+    player.draw(); //Draw player
 
-        SDL_SetRenderDrawColor(renderer, 0, 255, 255, 255);
-        SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+  }
 
-        SDL_RenderPresent(renderer);
-    }
-
-    return 0;
+  SDL_StopTextInput();
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
 }
