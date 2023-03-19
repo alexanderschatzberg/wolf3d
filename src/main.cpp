81cd8ed28bdfafe7ec8dc5d536031e7173fc9847
Alexander Schatzberg <alex1nderschatzberg@gmail.com>
2023-03-18 23:05:43 -0700

    Init SDL

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <SDL2/SDL.h> 
 
 int main(){
+
+  if(SDL_Init(SDL_INIT_EVERYTHING) > 0){
+    std::cout << "Shit failed" << std::endl; 
+  }
+
   std::string hello = "Hello world"; 
   std::cout << hello << std::endl; 
+  SDL_Quit(); 
   return 0; 
 }
